89a7df2963de666d20b0105585a363d0eee05178
SrBerth <elsradal120@gmail.com>
2025-09-03 21:02:33 -0500

    Add files via upload

---
+++ Unidad1/Estructuras_de_Control/ElNumeroEsPrimo_EC1/Program.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("--¿El numero es primo?--");
+
+        int intNumero = 0;
+        bool blnEntradaValida = false;
+
+        while (!blnEntradaValida)
+        {
+            Console.Write("Teclee un numero: ");
+
+            try
+            {
+                intNumero = Convert.ToInt32(Console.ReadLine());
+                blnEntradaValida = true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Ingrese solo datos numericos. Intente de nuevo.");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: El numero es demasiado grande. Intente de nuevo.");
+            }
+        }
+
+        if (intNumero % 2 == 0)
+        {
+            Console.WriteLine($"{intNumero} es un numero Primo");
+        }
+        else
+        {
+            Console.WriteLine($"{intNumero} no es un numero Primo");
+        }
+
+        Console.ReadKey();
+
+    }
+}
